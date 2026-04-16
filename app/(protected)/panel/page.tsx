1913a64c2fbc734f48d337bce1d9ac0910c72a35
natanaeldev20 <natanaelvasquez150103@gmail.com>
2026-04-16 08:57:24 -0500

    embarcadero-pesantes2

--- app/(protected)/panel/page.tsx
+++ app/(protected)/panel/page.tsx
@@ -1,9 +1,11 @@
 import { auth } from "@/auth";
+import { SiteHeader } from "@/global/components/site-header";
 
 export default async function PanelPage() {
   const session = await auth();
   return (
     <div className="p-4 text-center text-3xl font-bold">
+      <SiteHeader title="Inicio" />
       <h1 className="mt-20">{`Bienvenido al sistema ${session?.user?.name}`}</h1>
     </div>
   );

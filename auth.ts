62842b6038c8356f3f0a3bcc7603d385a72f7d92
natanaeldev20 <natanaelvasquez150103@gmail.com>
2026-04-29 23:19:16 -0500

    kitcodenata

--- auth.ts
+++ auth.ts
@@ -5,7 +5,11 @@ import authConfig from "./auth.config";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  session: { strategy: "jwt" },
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60,
+    updateAge: 24 * 60 * 60,
+  },
   ...authConfig,
   callbacks: {
     jwt({ token, user }) {
